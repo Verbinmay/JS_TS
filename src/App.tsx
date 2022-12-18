ed1068b6042d711532eb69c5d5308644e72b9745
Verbinmay <revanmayfall@gmail.com>
2022-12-19 00:01:04 +0400

    app.tst добавили в map key

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 const users = [{name:'maar'},{name:'ndfd'},{name:'hdds'}];
 
 //const liElements = names.map(n => <li>{n}</li>)
-const liElements = users.map(u => <li>{u.name}</li>)
+const liElements = users.map((u,i) => <li key={i}>{u.name}</li>)
 function App() {
   return (
     <div className="App">
